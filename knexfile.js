445591c4d8170550114d5d64268c3f691fdaf7d1
Paulyn <paulyntran@live.ca>
2019-02-28 22:23:59 +0000

    Updated knexfile with updated heroku pg link

--- knexfile.js
+++ knexfile.js
@@ -23,17 +23,17 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: 'postgres://tuomxgfnoevowk:6f9dfd9283a5de4df58f286829ffa1bc583d0491511ffc6db993fd644232eade@ec2-174-129-236-21.compute-1.amazonaws.com:5432/ddpcvb0gu1l7jg?ssl=true',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       tableName: 'migrations'
-    },
-    ssl: true
+    }
   }
 
 };
 
-
+//saving the things
+//get 
