0b9054190d1ac0eff58f3aecaa5695c34206e3de
CClarkes <connorclarkes@gmail.com>
2019-02-28 22:57:42 +0000

    Begining to add Jquery for ind. menu items

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,5 @@
-// Nav Bar
+$(document).ready(function() {
+
 $(window).scroll(function () {
     if ($(window).scrollTop() >= 50) {
     $('#nav-bar').css('background','rgb(189, 178, 165, 0.5)');
@@ -19,6 +20,40 @@ $(document).on('scroll', function () {
     }
   });
 
+  function renderMenuItem(data) {
+    let $menuItem = `<article class="menu-item">
+    <header class="food-name">
+      <span class="food-item">Spaghetti</span>                    //Add KNEX query here
+      <span class="food-price">
+        $19.99                                                    //Add KNEX query here
+        <form>
+            <div class="form-row align-items-center">
+              <div class="col-auto my-1">
+                <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                  <option selected>Choose...</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>                            //Add more options
+                </select>
+              </div>
+              <div class="col-auto my-1">
+                <button type="submit" class="btn btn-primary">Add</button>
+              </div>
+            </div>
+          </form>
+      </span>
+      </header>
+      
+      <section class="food-description">
+          <p>Tasty spaghtetti</p>                         //Add KNEX query here
+      </section>
+  </article>`
+
+
+  }
 
 // Append Users
   $(() => {
@@ -32,3 +67,4 @@ $(document).on('scroll', function () {
     });;
   });
 
+}
