a58b79c83c6ee75a0cdf0cdd06bb29e493e62f9b
Paulyn <paulyntran@live.ca>
2019-03-01 02:50:28 +0000

    Added more beautification factor with css

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 
 require('dotenv').config();
 
-// const PORT        = process.env.PORT || 8080;
-let port = process.env.PORT || 5000;
-if (port == null || port == "") {
-  port = 8000;
-}
+// // const PORT        = process.env.PORT || 8080;
+// let port = process.env.PORT || 5000;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
+
+const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
@@ -47,10 +49,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-// app.listen(PORT, () => {
-//   console.log("Example app listening on port " + PORT);
-// });
-app.listen(port);
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
+// app.listen(port);
 
-console.log("Example app listening on port" + port); 
+// console.log("Example app listening on port" + port); 
 
