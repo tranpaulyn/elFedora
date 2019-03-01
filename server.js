30aa1495e10a339ed264163057d7a544cd441868
arslanah99 <Arah09@hotmail.com>
2019-03-01 20:03:42 +0000

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 
-// const PORT        = process.env.PORT || 8080;
+const PORT        = process.env.PORT || 8080;
 // // const PORT        = process.env.PORT || 8080;
 // let port = process.env.PORT || 5000;
 // if (port == null || port == "") {
