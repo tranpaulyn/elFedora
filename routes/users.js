37fea8dd20ac33cd818c21ab874e34b32da2888a
arslanah99 <Arah09@hotmail.com>
2019-03-02 01:17:12 +0000

    Added page jumper on side

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,11 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    console.log("shjdfzkdfjhdfjhdbfjsdbgjhsbfjghdfjgjdhfgbhj")
     knex
       .select("*")
-      .orderBy('id')
       .from("users")
       .then((results) => {
-        res.json(results.sort((a , b) => a.id - b.id));
+        res.json(results);
     });
   });
 
