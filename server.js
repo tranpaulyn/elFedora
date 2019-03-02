baae1173353e1d407dbcfc505a17d5bca2db65ee
Paulyn <paulyntran@live.ca>
2019-03-02 01:52:48 +0000

    Updated Server.js

--- server.js
+++ server.js
@@ -2,13 +2,11 @@
 
 require('dotenv').config();
 
-// // const PORT        = process.env.PORT || 8080;
-// let port = process.env.PORT || 5000;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
-
-const PORT        = process.env.PORT || 8080;
+// const PORT        = process.env.PORT || 8080;
+let port = process.env.PORT || 5000;
+if (port == null || port == "") {
+  port = 8000;
+}
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
