af6b39185662a825838a5b0bed63375c04eab794
Paulyn <paulyntran@live.ca>
2019-03-02 02:11:18 +0000

    Push to change branches

--- server.js
+++ server.js
@@ -65,6 +65,12 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
+// app.listen(port);
+
+
 // Connor's Twillio Stuff 
 // SMS capability
 //First, message goes to Restaurant
