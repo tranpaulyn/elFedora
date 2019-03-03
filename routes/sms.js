da1fd74967da10b0a41d6f671e61593c2177f860
Paulyn <paulyntran@live.ca>
2019-03-03 01:46:49 +0000

    Made changes to sms.js

--- routes/sms.js
+++ routes/sms.js
@@ -5,17 +5,14 @@ const router  = express.Router();
 let accountSid = process.env.TWILIO_ACCOUNT_SID;
 let authToken = process.env.TWILIO_AUTH_TOKEN;
 let client = require('twilio')(accountSid, authToken);
+let MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 
 module.exports = (knex) => {
 
-
-    // Connor's Twillio Stuff 
-// SMS capability
-
   //Then, message is sent from kitchen to server indicating cook-time.
-  router.post('/sms', (req, res) => {
-    let MessagingResponse = require('twilio').twiml.MessagingResponse;
+  router.post('/', (req, res) => {
+
     let twiml = new MessagingResponse();
     console.log(req);
     twiml.message('The Robots are coming! Head for the hills!');
